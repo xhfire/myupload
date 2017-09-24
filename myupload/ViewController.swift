177f010e69c4b7a9abaf865046c62f95a08df7a5
X H <xhfire@gmail.com>
2017-09-24 17:38:15 +0800

    Initial Commit

---
+++ myupload/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  myupload
+//
+//  Created by X H on 2017/9/24.
+//  Copyright © 2017年 X H. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
